40541450b343d6a93a4e3e5dc96062e233b528c0
Meenakshi Mohapatra <mohapatraminakshi07@gmail.com>
2025-03-06 04:56:35 +0000

    done the pdf development by using model

--- pdfupload/webapp/controller/View1.controller.js
+++ pdfupload/webapp/controller/View1.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/PDFViewer"
-], (Controller,PDFViewer) => {
+    "sap/m/PDFViewer",
+    "sap/ui/model/json/JSONModel"
+], (Controller,PDFViewer,JSONModel) => {
     "use strict";
 
     return Controller.extend("com.pdfupload.pdfupload.controller.View1", {
@@ -10,12 +11,24 @@ sap.ui.define([
                 isTrustedSource: true
             })
             this.getView().addDependent(this.pdfViewer);
+            this._sValidPath=sap.ui.require.toUrl("com/pdfupload/pdfupload/pdfFolder/Duppy_Conqueror_Press_Kit-1.pdf")
+            this._oModel=new JSONModel({
+                Source:this._sValidPath,
+                Title:"PDF Document",
+                Height:"600px"
+            })
+            this.getView().setModel(this._oModel);
+            
+            
         },
         onClickFn: function(){
             var validPath=sap.ui.require.toUrl("com/pdfupload/pdfupload/pdfFolder/Duppy_Conqueror_Press_Kit-1.pdf")
             this.pdfViewer.setSource(validPath);
             this.pdfViewer.setTitle("PDF DOCUMENTS");
             this.pdfViewer.open();
+            this._oModel.setProperty("/Source",this._sValidPath);
+           
+
         },
         onClickFragment: function(){
             if(!this.openDialog){
@@ -24,6 +37,13 @@ sap.ui.define([
             }
             this.openDialog.open();
             
+        },
+        openPdfviewer: function(){
+            if(!this.openPdfFile){
+                this.openPdfFile=sap.ui.xmlfragment("com.pdfupload.pdfupload.fragment.pdfViewer",this);
+                this.getView().addDependent(this.openPdfFile);
+            }
+            this.openPdfFile.open();
         },
         onClose: function(){
             this.openDialog.close();
